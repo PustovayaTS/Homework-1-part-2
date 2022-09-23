64a12c374fae41a879302a997b17fd6b3321d612
PustovayaTS <ais_ta@bk.ru>
2022-09-23 18:58:55 +0700

    Задание 1,2,3 блок 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задание №1");
+        int savings = 15000;
+        int total = 0;
+        int i = 0;
+        while (total < 2_459_000) {
+            i++;
+            total = total + total/100;
+            total = total + savings;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Задание №2");
+        int v = 0;
+        while (v < 10) {
+            v++;
+            System.out.print(v);
+        }
+        System.out.println();
+        for (int k = 10; k >=1; k--) {
+            System.out.print(k);
+        }
+        System.out.println();
+        System.out.println("Задание №3");
+        int totalPopulation = 12_000_000;
+        int birthRate = totalPopulation / 1000 * 17;
+        int mortality = totalPopulation / 1000 * 8;
+        for (int y = 0; y <= 10; y++) {
+            totalPopulation = totalPopulation + birthRate - mortality;
+            System.out.println("Год " + y + ", численность населения составляет " + totalPopulation);
+        }
     }
+
 }
